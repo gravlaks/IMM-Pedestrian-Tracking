c5e3487081709b25a56f9c8341d1947c4118e248
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2022-05-22 14:43:55 -0700

    range bearing fix

--- measurement_models/range_bearing.py
+++ measurement_models/range_bearing.py
@@ -16,17 +16,15 @@ class RangeBearing():
 
         return np.vstack(
             (np.linalg.norm(p), 
-            np.arctan(p[1]/p[0])
+            np.arctan2(p[1], p[0])
             )
         )
 
     def H(self, x):
         #http://www.cs.cmu.edu/~16385/s17/Slides/16.4_Extended_Kalman_Filter.pdf
         p = x[:2]
-        if p[0] == 0:
-            theta = np.sign(p[1])*np.pi/2
-        else:
-            theta = np.arctan(p[1]/p[0])
+        
+        theta = np.arctan2(p[1], p[0])
         r = np.linalg.norm(p)
         H = np.zeros((2, self.state_dim))
         H[0, 0] = np.cos(theta)
