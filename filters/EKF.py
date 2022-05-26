8f8eafeb83cb83abae3d3c303e54b734a15929ae
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2022-05-25 21:59:52 -0700

    ekf joseph form implemented

--- filters/EKF.py
+++ filters/EKF.py
@@ -2,6 +2,7 @@ import numpy as np
 from measurement_models.range_bearing import RangeBearing
 from utils.Gauss import GaussState
 import scipy.stats
+import scipy.linalg
 class EKF():
     def __init__(self, dynamics_model, measurement_model):
         self.dyn_model = dynamics_model
@@ -39,8 +40,20 @@ class EKF():
             innovation[1] = self.wrap_angle(innovation[1])
         else: 
             raise Exception( "Should use RangeBearing model")
-        mu_nxt = mu+S@C.T@np.linalg.solve(C@S@C.T + R, y-y_hat)
-        Sigma_nxt = S - S@C.T@np.linalg.solve(C@S@C.T + R, C@S)
+        joseph_form = False
+        if joseph_form:
+            K = S@np.linalg.solve((C@S@C.T+R), C).T
+            mu_nxt = mu + K@ innovation
+            I = np.eye(((K@C).shape[0]))
+            Sigma_nxt = (I - K @ C) @ S @ (I- K @C).T + K@ R @ K.T
+        else:
+            mu_nxt = mu+S@C.T@np.linalg.solve(C@S@C.T + R, y-y_hat)
+            Sigma_nxt = S - S@C.T@np.linalg.solve(C@S@C.T + R, C@S)
+        #if np.any(np.linalg.eigvals(Sigma_nxt)<0):
+            #Sigma_nxt += np.eye(Sigma_nxt.shape[0])*1e-1
+            #import pdb;pdb.set_trace()
+
+            #raise Exception
         assert(mu.shape==mu_nxt.shape)
         if np.isnan(mu_nxt[0,0]):
             import pdb;pdb.set_trace()
@@ -55,6 +68,8 @@ class EKF():
 
         """
 
+        self._MLOG2PIby2 = 2* \
+            np.log(2 * np.pi) / 2
         mean, cov = gauss_state
         innov = y-self.meas_model.h(mean)
 
@@ -66,11 +81,23 @@ class EKF():
         # rv = multivariate_normal(np.zeros_like(y), R)
 
         #     L = rv.pdf(y - self.meas_model.h(x))
+
+        # cholS = scipy.linalg.cholesky(S, lower=True)
+    
+        # invcholS_v = scipy.linalg.solve_triangular(cholS, innov, lower=True)
+        # NISby2 = (invcholS_v ** 2).sum() / 2
+        # # alternative self.NIS(...) /2 or v @ la.solve(S, v)/2
+    
+        # logdetSby2 = np.log(cholS.diagonal()).sum()
+        # # alternative use la.slogdet(S)
+    
+        # ll = -(NISby2 + logdetSby2 + self._MLOG2PIby2)
         try:
             ll = scipy.stats.multivariate_normal.logpdf(innov.flatten(), cov=S, allow_singular=True)
         except:
             # sometimes it says S is singular when it isn't - what we can do is feed an argument allow_singular=True
             import pdb;pdb.set_trace()
+            #ll = -1000
         #ll = max(-40, ll)
 
         # if ll<-1000.:
