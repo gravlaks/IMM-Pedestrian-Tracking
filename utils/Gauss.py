9fdede293d357a21e5a5cfe25d8e491acd9fc570
Brian Dobkowski <bdobkowski14@comcast.net>
2022-05-29 17:58:35 -0700

    making acc state augmentation more robust

--- utils/Gauss.py
+++ utils/Gauss.py
@@ -11,6 +11,32 @@ class GaussState:
     def __iter__(self):
         return iter((self.mean, self.cov))
 
+def adjust_acc_averages(idx_ct, idx_ca, means, covs, weights):
+    w = []
+    means_acc = []
+    covs_acc = []
+
+    weights = np.copy(weights.flatten())
+
+    for ict in idx_ct:
+        w.append(weights[ict])
+        means_acc.append(means[ict, 4:])
+        covs_acc.append(covs[ict, 4:, 4:])
+
+    for ica in idx_ca:
+        w.append(weights[ica])
+        means_acc.append(means[ica, 4:])
+        covs_acc.append(covs[ica, 4:, 4:])
+
+    weights_acc = np.array(w/np.sum(w))
+    acc_mean = np.array(means_acc)
+    acc_cov = np.array(covs_acc)
+
+    mean_bar_acc = np.average(acc_mean, weights=weights_acc, axis=0).reshape(-1,1)
+    cov_left_acc = np.average(acc_cov, weights=weights_acc, axis=0)
+
+    return mean_bar_acc, cov_left_acc
+
 
 def moments_gaussian_mixture(weights, means, covs, dyn_type):
     """
@@ -28,17 +54,27 @@ def moments_gaussian_mixture(weights, means, covs, dyn_type):
     idx_ct = [i for i in range(len(dyn_type)) if dyn_type[i]=='CT']
     idx_ca = [i for i in range(len(dyn_type)) if dyn_type[i]=='CA']
 
-    if len(idx_ct) > 0 and len(idx_ca) ==0:
-        mean_bar[4:, :] = means[idx_ct, 4:].T
-        cov_left[4:, 4:] = covs[idx_ct, 4:, 4:]
-    elif len(idx_ca) > 0 and len(idx_ct) ==0:
-        mean_bar[4:, :] = means[idx_ca, 4:].T
-        cov_left[4:, 4:] = covs[idx_ca, 4:, 4:]
-    elif len(idx_ct) == 0 and len(idx_ca) == 0:
-        pass
-    else:
-        print('averaging accelerations and omega for CT and CA together not yet implemented')
+    # if len(idx_ct) > 0 and len(idx_ca) ==0:
+    #     mean_bar[4:, :] = means[idx_ct, 4:].T
+    #     cov_left[4:, 4:] = covs[idx_ct, 4:, 4:]
+    # elif len(idx_ca) > 0 and len(idx_ct) ==0:
+    #     mean_bar[4:, :] = means[idx_ca, 4:].T
+    #     cov_left[4:, 4:] = covs[idx_ca, 4:, 4:]
+    # elif len(idx_ct) == 0 and len(idx_ca) == 0:
+    #     pass
+    # else:
+    #     # print('averaging accelerations and omega for CT and CA together not yet implemented')
+    #     w_ct = weights_flat[idx_ct]/(weights_flat[idx_ct] + weights_flat[idx_ca])
+    #     w_ca = weights_flat[idx_ca]/(weights_flat[idx_ct] + weights_flat[idx_ca])
+    #     weights_acc = np.array([w_ct, w_ca]).flatten()
+    #     acc_mean = np.concatenate((means[idx_ct, 4:], means[idx_ca, 4:]))
+    #     acc_cov = np.concatenate((covs[idx_ct, 4:, 4:], covs[idx_ca, 4:, 4:]))
+    #     mean_bar[4:, :] = np.average(acc_mean, weights=weights_acc, axis=0).reshape(-1,1)
+    #     cov_left[4:, 4:] = np.average(acc_cov, weights=weights_acc, axis=0)
+    #     # pass
 
+    if len(idx_ct) > 0 or len(idx_ca) > 0:
+        mean_bar[4:, :], cov_left[4:, 4:] = adjust_acc_averages(idx_ct, idx_ca, means, covs, weights)
 
     #spread of innovations
 
